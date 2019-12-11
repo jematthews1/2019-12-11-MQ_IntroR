b3f56eff8296314d8a9b6c9b89d2e93cd69cb365
jematthews1 <evan.matthews1@hdr.mq.edu.au>
2019-12-11 16:07:19 +1100

    didn't quite finish the intro to data part

--- scripts/Introduction.R
+++ scripts/Introduction.R
@@ -5,8 +5,22 @@ print('Hello, World')
 print('Hello, World')
 
 # this is how we install multiple packages at once
-install.packages(c("bigmemory", "bit64"))
-library(tidyverse)
+# install.packages(c("bigmemory", "bit64"))
+# library(tidyverse)
+# 
+# area_hectares2 <- 24
+# print(area_hectares2)
+# 
+# (area_hectares3 <- 30)
+
+#################
+# Analysis of land #
+####################
+
+some code
+#--------------------------
+
+
 
 
 
