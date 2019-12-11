2cc931461ca1542f150bd47de6f7ae3b6bcf2d69
jematthews1 <evan.matthews1@hdr.mq.edu.au>
2019-12-11 15:03:33 +1100

    introduction to R script

---
+++ scripts/Introduction.R
@@ -0,0 +1,13 @@
+# introduction to R
+
+print('Hello, World')
+print('Hello, World')
+print('Hello, World')
+
+# this is how we install multiple packages at once
+install.packages(c("bigmemory", "bit64"))
+library(tidyverse)
+
+
+
+
